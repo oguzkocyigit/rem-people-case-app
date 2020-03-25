3b96c978673224f48be1680e57bb083b396d235b
oguzkocyigit <kocyigitoguz90@gmail.com>
2020-03-25 16:39:54 +0300

    deleted unnecessary element.io components

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,20 +2,12 @@ import Vue from 'vue'
 import 'element-ui/lib/theme-chalk/index.css'
 import {
   Input,
-  Checkbox,
-  Radio,
   Table,
   TableColumn,
   Button,
-  Loading
 } from 'element-ui'
 
 Vue.use(Input)
-Vue.use(Checkbox)
 Vue.use(Button)
-Vue.use(Radio)
 Vue.use(Table)
 Vue.use(TableColumn)
-Vue.use(Loading.directive)
-
-Vue.prototype.$loading = Loading.service
